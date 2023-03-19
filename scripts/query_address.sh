439e9658a56f867bd3176a6ef563738159899838
Niels Mündler <n.muendler@posteo.de>
2023-03-19 09:48:58 +0100

    Remove need for the CLI

--- scripts/query_address.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-cardano_cli="docker run --rm -it \
-  --entrypoint cardano-cli \
-  -e CARDANO_NODE_SOCKET_PATH=/ipc/node.socket \
-  -v opshin-pioneer-program_node-db:/data \
-  -v opshin-pioneer-program_node-ipc:/ipc \
-  inputoutput/cardano-node"
-$cardano_cli query utxo --address "$1" --testnet-magic 2
--- scripts/query_tip.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-cardano_cli="docker run --rm -it \
-  --entrypoint cardano-cli \
-  -e CARDANO_NODE_SOCKET_PATH=/ipc/node.socket \
-  -v opshin-pioneer-program_node-db:/data \
-  -v opshin-pioneer-program_node-ipc:/ipc \
-  inputoutput/cardano-node"
-$cardano_cli query tip --testnet-magic 2
