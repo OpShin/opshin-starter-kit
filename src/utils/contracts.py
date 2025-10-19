c942eba0d7cd7eee7386b29d4b53d516bb40661e
Niels <niels.muendler@inf.ethz.ch>
2025-10-19 14:07:09 +0200

    Adapt to mainnet

--- src/utils/contracts.py
+++ src/utils/contracts.py
@@ -2,8 +2,10 @@ from pathlib import Path
 
 from pycardano import plutus_script_hash, Address, Network, PlutusV3Script
 
+from src.utils.network import network
 
-def get_contract(name: str, network=Network.TESTNET):
+
+def get_contract(name: str, network=network):
     # Load script info about a contract built with opshin
     script_path = Path(f"./build/{name}/script.cbor")
     with open(script_path) as f:
--- src/utils/keys.py
+++ src/utils/keys.py
@@ -2,16 +2,22 @@ from pathlib import Path
 
 from pycardano import PaymentVerificationKey, PaymentSigningKey, Address, Network
 
+from src.utils.network import network
+
 keys_dir = Path(__file__).parent.parent.parent.joinpath("keys")
 
 
-def get_address(name) -> Address:
+def get_address(name, network=network) -> Address:
     with open(keys_dir.joinpath(f"{name}.addr")) as f:
         address = Address.from_primitive(f.read())
-    return address
+    return Address(
+        address.payment_part,
+        address.staking_part,
+        network=network,
+    )
 
 
-def get_signing_info(name, network=Network.TESTNET):
+def get_signing_info(name, network=network):
     skey_path = str(keys_dir.joinpath(f"{name}.skey"))
     payment_skey = PaymentSigningKey.load(skey_path)
     payment_vkey = PaymentVerificationKey.from_signing_key(payment_skey)
